843d9f8605a7120188569e085c36218f68bc3679
MelekZam <zamourimelek@gmail.com>
2021-03-27 13:54:31 +0100

    initializing the project

---
+++ android/app/src/main/java/com/na_monitoring/MainActivity.java
@@ -0,0 +1,15 @@
+package com.na_monitoring;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "na_monitoring";
+  }
+}
